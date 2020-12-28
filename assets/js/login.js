0bba035374f4827548dd49e42424adec4bdb6499
Tz <2215690540@qq.com>
2020-12-28 21:22:10 +0800

    登录页面请求完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,7 @@ $(function () {
     })
 
     var form = layui.form
+    var layer=layui.layer
     form.verify({
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repwd: function (value) {
@@ -19,17 +20,43 @@ $(function () {
                 return '两次输入的密码不一致'
             }
         }
-
-
     })
     // 监听注册表单的提交事件
     $('#form_reg').on('submit', function (e) {
+        // 阻止默认提交
         e.preventDefault();
-        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, function (res) {
-            if (res.status !== 0) {
-            return console.log(res.message);
+        var data={
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser',data,
+             function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功');
+
+                // 模拟点击行为
+                $('#link_login').click()
+            })
+    })
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'post',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败');
+                }
+                layer.msg('登录成功');
+                localStorage.setItem('token',res.token)
+                location.href='/index.html'
             }
-            console.log('注册成功');
+                
+            
         })
     })
 })
